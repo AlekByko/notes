6ff5aa2736f86ece4faa366474412cb9baaad32f
AlekByko <>
2019-07-06 14:09:19 -0500

    initial setup

---
+++ require-config.js
@@ -0,0 +1,8 @@
+var require  = {
+    baseUrl: './scripts',
+    paths: {
+        'react': '../node_modules/react/umd/react.development',
+        'react-dom': '../node_modules/react-dom/umd/react-dom.development',
+        'three': '../node_modules/three/build/three'
+    }
+};
