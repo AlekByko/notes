f7af56f47bc05dff21754b97c13603de0eaa2cde
AlekByko <>
2019-07-09 17:38:43 -0500

    external configs

--- require-config.js
+++ require-config.js
@@ -1,8 +1,13 @@
 var require  = {
-    baseUrl: './scripts',
+    baseUrl: './',
     paths: {
-        'react': '../node_modules/react/umd/react.development',
-        'react-dom': '../node_modules/react-dom/umd/react-dom.development',
-        'three': '../node_modules/three/build/three'
-    }
+        'text': './node_modules/requirejs-plugins/lib/text',
+        'json': './node_modules/requirejs-plugins/src/json',
+        'react': './node_modules/react/umd/react.development',
+        'react-dom': './node_modules/react-dom/umd/react-dom.development'
+    },
+    packages: [
+	{ name: 'saving', location: './scripts/saving', main: 'configs' },
+        { name: 'configs', location: './configs', main: 'configs' }
+    ]
 };
