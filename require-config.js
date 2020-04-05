a68f17240e49223a03c7183b6967ced91b767ac4
AlekByko <>
2020-04-05 15:14:48 -0500

    tensorflow loaded in the browser

--- require-config.js
+++ require-config.js
@@ -4,7 +4,10 @@ var require  = {
         'text': './node_modules/requirejs-plugins/lib/text',
         'json': './node_modules/requirejs-plugins/src/json',
         'react': './node_modules/react/umd/react.development',
-        'react-dom': './node_modules/react-dom/umd/react-dom.development'
+        'react-dom': './node_modules/react-dom/umd/react-dom.development',
+        '@tensorflow/tfjs': './node_modules/@tensorflow/tfjs/dist/tf.min',
+    },
+    shims: {
     },
     packages: [
 	{ name: 'saving', location: './scripts/saving', main: 'configs' },
