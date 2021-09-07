07b67c8035398126a70cf56a9c5cdc9c7a73fa15
AlekByko <>
2021-09-06 19:50:44 -0500

    "better" rounding

--- saving/shared/coloring.ts
+++ saving/shared/coloring.ts
@@ -21,42 +21,37 @@ export function toHue(r: number, g: number, b: number): number | null {
     return h;
 }
 
-function hue2rgb(p: number, q: number, t: number) {
-    if (t < 0) t += 1;
-    if (t > 1) t -= 1;
-    if (t < 1 / 6) return p + (q - p) * 6 * t;
-    if (t < 1 / 2) return q;
-    if (t < 2 / 3) return p + (q - p) * (2 / 3 - t) * 6;
-    p = p * 255;
-    p = Math.round(p);
-    return Math.min(p, 255);
-}
 
-export function hslToRgb(h: number, s: number, l: number){
+export function hslToRgb(h: number, s: number, l: number) {
     var r, g, b;
 
-    if(s == 0){
+    if (s == 0) {
         r = g = b = l; // achromatic
-    }else{
-        var hue2rgb = function hue2rgb(p: number, q: number, t: number){
-            if(t < 0) t += 1;
-            if(t > 1) t -= 1;
-            if(t < 1/6) return p + (q - p) * 6 * t;
-            if(t < 1/2) return q;
-            if(t < 2/3) return p + (q - p) * (2/3 - t) * 6;
+    } else {
+        var hue2rgb = function hue2rgb(p: number, q: number, t: number) {
+            if (t < 0) t += 1;
+            if (t > 1) t -= 1;
+            if (t < 1 / 6) return p + (q - p) * 6 * t;
+            if (t < 1 / 2) return q;
+            if (t < 2 / 3) return p + (q - p) * (2 / 3 - t) * 6;
             return p;
         }
 
         var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
         var p = 2 * l - q;
-        r = hue2rgb(p, q, h + 1/3);
+        r = hue2rgb(p, q, h + 1 / 3);
         g = hue2rgb(p, q, h);
-        b = hue2rgb(p, q, h - 1/3);
+        b = hue2rgb(p, q, h - 1 / 3);
     }
 
-    return [Math.round(r * 255), Math.round(g * 255), Math.round(b * 255)];
+    return [round(r, 255), round(g, 255), round(b, 255)];
 }
 
-export function rgbToHex(r:number, g: number, b: number): string {
+function round(value: number, size: number): number {
+    const almost = Math.floor(value * (size + 1));
+    return Math.min(almost, size);
+}
+
+export function rgbToHex(r: number, g: number, b: number): string {
     return '#' + padZeroText(2, r.toString(16)) + padZeroText(2, g.toString(16)) + padZeroText(2, b.toString(16));
 }
