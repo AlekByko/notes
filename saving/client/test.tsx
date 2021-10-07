db636a46b03cf592678e5a7cf7e74bf74e328402
AlekByko <>
2021-10-06 19:57:24 -0500

    tensorflow disabled

--- saving/client/test.tsx
+++ saving/client/test.tsx
@@ -1,150 +1,150 @@
-import * as tf from '@tensorflow/tfjs';
-import * as tfvis from '@tensorflow/tfjs-vis';
-import { MnistData } from './data';
+// import * as tf from '@tensorflow/tfjs';
+// import * as tfvis from '@tensorflow/tfjs-vis';
+// import { MnistData } from './data';
 
-function xxx() {
+// function xxx() {
 
-    async function showExamples(data: MnistData) {
-        // Create a container in the visor
-        const surface =
-            tfvis.visor().surface({ name: 'Input Data Examples', tab: 'Input Data' });
+//     async function showExamples(data: MnistData) {
+//         // Create a container in the visor
+//         const surface =
+//             tfvis.visor().surface({ name: 'Input Data Examples', tab: 'Input Data' });
 
-        // Get the examples
-        const examples = data.nextTestBatch(20);
-        const numExamples = examples.xs.shape[0];
+//         // Get the examples
+//         const examples = data.nextTestBatch(20);
+//         const numExamples = examples.xs.shape[0];
 
-        // Create a canvas element to render each example
-        for (let i = 0; i < numExamples; i++) {
-            const imageTensor = tf.tidy(() => {
-                // Reshape the image to 28x28 px
-                return examples.xs
-                    .slice([i, 0], [1, examples.xs.shape[1]])
-                    .reshape([28, 28, 1]);
-            });
+//         // Create a canvas element to render each example
+//         for (let i = 0; i < numExamples; i++) {
+//             const imageTensor = tf.tidy(() => {
+//                 // Reshape the image to 28x28 px
+//                 return examples.xs
+//                     .slice([i, 0], [1, examples.xs.shape[1]])
+//                     .reshape([28, 28, 1]);
+//             });
 
-            const canvas = document.createElement('canvas');
-            canvas.width = 28;
-            canvas.height = 28;
-            await tf.browser.toPixels(imageTensor as any, canvas);
-            surface.drawArea.appendChild(canvas);
+//             const canvas = document.createElement('canvas');
+//             canvas.width = 28;
+//             canvas.height = 28;
+//             await tf.browser.toPixels(imageTensor as any, canvas);
+//             surface.drawArea.appendChild(canvas);
 
-            imageTensor.dispose();
-        }
-    }
+//             imageTensor.dispose();
+//         }
+//     }
 
-    async function run() {
-        const data = new MnistData();
-        await data.load();
-        await showExamples(data);
-        const model = getModel();
-        tfvis.show.modelSummary({ name: 'Model Architecture' }, model);
+//     async function run() {
+//         const data = new MnistData();
+//         await data.load();
+//         await showExamples(data);
+//         const model = getModel();
+//         tfvis.show.modelSummary({ name: 'Model Architecture' }, model);
 
-        await train(model, data);
-        const saved = await model.save('test.x');
-        void saved;
-    }
+//         await train(model, data);
+//         const saved = await model.save('test.x');
+//         void saved;
+//     }
 
-    run();
-}
-
-
-function getModel() {
-    const model = tf.sequential();
-
-    const IMAGE_WIDTH = 28;
-    const IMAGE_HEIGHT = 28;
-    const IMAGE_CHANNELS = 1;
-
-    // In the first layer of our convolutional neural network we have
-    // to specify the input shape. Then we specify some parameters for
-    // the convolution operation that takes place in this layer.
-    model.add(tf.layers.conv2d({
-        inputShape: [IMAGE_WIDTH, IMAGE_HEIGHT, IMAGE_CHANNELS],
-        kernelSize: 5,
-        filters: 8,
-        strides: 1,
-        activation: 'relu',
-        kernelInitializer: 'varianceScaling'
-    }));
-
-    // The MaxPooling layer acts as a sort of downsampling using max values
-    // in a region instead of averaging.
-    model.add(tf.layers.maxPooling2d({ poolSize: [2, 2], strides: [2, 2] }));
-
-    // Repeat another conv2d + maxPooling stack.
-    // Note that we have more filters in the convolution.
-    model.add(tf.layers.conv2d({
-        kernelSize: 5,
-        filters: 16,
-        strides: 1,
-        activation: 'relu',
-        kernelInitializer: 'varianceScaling'
-    }));
-    model.add(tf.layers.maxPooling2d({ poolSize: [2, 2], strides: [2, 2] }));
-
-    // Now we flatten the output from the 2D filters into a 1D vector to prepare
-    // it for input into our last layer. This is common practice when feeding
-    // higher dimensional data to a final classification output layer.
-    model.add(tf.layers.flatten());
-
-    // Our last layer is a dense layer which has 10 output units, one for each
-    // output class (i.e. 0, 1, 2, 3, 4, 5, 6, 7, 8, 9).
-    const NUM_OUTPUT_CLASSES = 10;
-    model.add(tf.layers.dense({
-        units: NUM_OUTPUT_CLASSES,
-        kernelInitializer: 'varianceScaling',
-        activation: 'softmax'
-    }));
+//     run();
+// }
 
 
-    // Choose an optimizer, loss function and accuracy metric,
-    // then compile and return the model
-    const optimizer = tf.train.adam();
-    model.compile({
-        optimizer: optimizer,
-        loss: 'categoricalCrossentropy',
-        metrics: ['accuracy'],
-    });
+// function getModel() {
+//     const model = tf.sequential();
 
-    return model;
-}
+//     const IMAGE_WIDTH = 28;
+//     const IMAGE_HEIGHT = 28;
+//     const IMAGE_CHANNELS = 1;
+
+//     // In the first layer of our convolutional neural network we have
+//     // to specify the input shape. Then we specify some parameters for
+//     // the convolution operation that takes place in this layer.
+//     model.add(tf.layers.conv2d({
+//         inputShape: [IMAGE_WIDTH, IMAGE_HEIGHT, IMAGE_CHANNELS],
+//         kernelSize: 5,
+//         filters: 8,
+//         strides: 1,
+//         activation: 'relu',
+//         kernelInitializer: 'varianceScaling'
+//     }));
+
+//     // The MaxPooling layer acts as a sort of downsampling using max values
+//     // in a region instead of averaging.
+//     model.add(tf.layers.maxPooling2d({ poolSize: [2, 2], strides: [2, 2] }));
+
+//     // Repeat another conv2d + maxPooling stack.
+//     // Note that we have more filters in the convolution.
+//     model.add(tf.layers.conv2d({
+//         kernelSize: 5,
+//         filters: 16,
+//         strides: 1,
+//         activation: 'relu',
+//         kernelInitializer: 'varianceScaling'
+//     }));
+//     model.add(tf.layers.maxPooling2d({ poolSize: [2, 2], strides: [2, 2] }));
+
+//     // Now we flatten the output from the 2D filters into a 1D vector to prepare
+//     // it for input into our last layer. This is common practice when feeding
+//     // higher dimensional data to a final classification output layer.
+//     model.add(tf.layers.flatten());
+
+//     // Our last layer is a dense layer which has 10 output units, one for each
+//     // output class (i.e. 0, 1, 2, 3, 4, 5, 6, 7, 8, 9).
+//     const NUM_OUTPUT_CLASSES = 10;
+//     model.add(tf.layers.dense({
+//         units: NUM_OUTPUT_CLASSES,
+//         kernelInitializer: 'varianceScaling',
+//         activation: 'softmax'
+//     }));
 
 
-async function train(model: tf.Sequential, data: MnistData) {
-    const metrics = ['loss', 'val_loss', 'acc', 'val_acc'];
-    const container = {
-        name: 'Model Training', styles: { height: '1000px' }
-    };
-    const fitCallbacks = tfvis.show.fitCallbacks(container, metrics);
+//     // Choose an optimizer, loss function and accuracy metric,
+//     // then compile and return the model
+//     const optimizer = tf.train.adam();
+//     model.compile({
+//         optimizer: optimizer,
+//         loss: 'categoricalCrossentropy',
+//         metrics: ['accuracy'],
+//     });
 
-    const BATCH_SIZE = 512;
-    const TRAIN_DATA_SIZE = 5500;
-    const TEST_DATA_SIZE = 1000;
+//     return model;
+// }
 
-    const [trainImages, trainLabels] = tf.tidy(() => {
-        const d = data.nextTrainBatch(TRAIN_DATA_SIZE);
-        return [
-            d.xs.reshape([TRAIN_DATA_SIZE, 28, 28, 1]),
-            d.labels
-        ];
-    });
 
-    const [testImages, testLabels] = tf.tidy(() => {
-        const d = data.nextTestBatch(TEST_DATA_SIZE);
-        return [
-            d.xs.reshape([TEST_DATA_SIZE, 28, 28, 1]),
-            d.labels
-        ];
-    });
+// async function train(model: tf.Sequential, data: MnistData) {
+//     const metrics = ['loss', 'val_loss', 'acc', 'val_acc'];
+//     const container = {
+//         name: 'Model Training', styles: { height: '1000px' }
+//     };
+//     const fitCallbacks = tfvis.show.fitCallbacks(container, metrics);
 
-    return model.fit(trainImages, trainLabels, {
-        batchSize: BATCH_SIZE,
-        validationData: [testImages, testLabels],
-        epochs: 10,
-        shuffle: true,
-        callbacks: fitCallbacks
-    });
-}
+//     const BATCH_SIZE = 512;
+//     const TRAIN_DATA_SIZE = 5500;
+//     const TEST_DATA_SIZE = 1000;
 
-xxx();
+//     const [trainImages, trainLabels] = tf.tidy(() => {
+//         const d = data.nextTrainBatch(TRAIN_DATA_SIZE);
+//         return [
+//             d.xs.reshape([TRAIN_DATA_SIZE, 28, 28, 1]),
+//             d.labels
+//         ];
+//     });
+
+//     const [testImages, testLabels] = tf.tidy(() => {
+//         const d = data.nextTestBatch(TEST_DATA_SIZE);
+//         return [
+//             d.xs.reshape([TEST_DATA_SIZE, 28, 28, 1]),
+//             d.labels
+//         ];
+//     });
+
+//     return model.fit(trainImages, trainLabels, {
+//         batchSize: BATCH_SIZE,
+//         validationData: [testImages, testLabels],
+//         epochs: 10,
+//         shuffle: true,
+//         callbacks: fitCallbacks
+//     });
+// }
+
+// xxx();
 
