e528b9000a0cf9dd2fa7dce312c7442f37868bf3
AlekByko <>
2022-01-07 20:45:59 -0500

    texter component

---
+++ saving/client/texter.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Regarding } from './reacting';
+
+interface State {
+    text: string;
+}
+export interface TexterProps<Concern> {
+    regarding: Regarding<Concern>;
+}
+
+export function thusTexter<Concern>(toConcern: (text: string) => Concern) {
+    return class Texter extends React.Component<TexterProps<Concern>, State> {
+        private whenChangedText: React.ChangeEventHandler<HTMLInputElement> = e => {
+            const text = e.currentTarget.value;
+            this.setState({ text });
+        }
+        private whenKeyDown: React.KeyboardEventHandler<HTMLInputElement> = e => {
+            if (e.key === 'Enter') {
+                this.props.regarding(toConcern(this.state.text));
+            }
+        }
+        render() {
+            return <input onKeyDown={this.whenKeyDown} onChange={this.whenChangedText} />;
+        }
+    };
+}
