3625f730bed2455862c3d82eedb822e349fa02ae
AlekByko <>
2023-02-04 18:37:03 -0500

    number value editor

---
+++ saving/client/numeric-value-editor.tsx
@@ -0,0 +1,33 @@
+import * as React from 'react';
+import { to } from './shared/core';
+
+export interface NumbericValueEditortProps {
+    value: number;
+    context: unknown;
+    onChange: (value: number) => void;
+}
+
+interface State {
+    text: string;
+}
+
+export class NumericValueEditor extends React.Component<NumbericValueEditortProps> {
+    state = to<State>({ text: String(this.props.value) });
+    render() {
+        const { onChange } = this.props;
+        const { text } = this.state;
+        return <input
+            value={text}
+            onChange={e => {
+                const text = e.currentTarget.value;
+                this.setState({ text });
+            }}
+            onBlur={e => {
+                const text = e.currentTarget.value
+                const value = Number(text);
+                if (!isFinite(value)) return;
+                onChange(value);
+            }}
+        />;
+    }
+}
