76f242fd097c3c4009b4d0357bfedcda90d6e2b0
AlekByko <>
2023-02-11 18:34:20 -0500

    measured value editor

---
+++ saving/client/measured-value-editor.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { ReactConstructor } from './reacting';
+
+export interface MeasuredValue<Value, Unit> {
+    value: Value;
+    unit: Unit;
+}
+
+export interface MeasuredValueEditorProps<Value, Unit, Context> {
+    measured: MeasuredValue<Value, Unit>;
+    context: Context;
+    onChange: (measured: MeasuredValue<Value, Unit>) => void;
+}
+
+interface ValueEditorProps<Value, Context> { // should not be exported, for internal use only, works as a constraint
+    value: Value;
+    context: Context;
+    onChange: (value: Value) => void;
+}
+
+interface UnitEditorProps<Unit, Context> { // should not be exported, for internal use only, works as a constraint
+    value: Unit;
+    context: Context;
+    onChange: (value: Unit) => void;
+}
+
+export function thusMeasuredValueEditor<Value, Unit, Context>(
+    ValueEditor: ReactConstructor<ValueEditorProps<Value, Context>>,
+    UnitEditor: ReactConstructor<UnitEditorProps<Unit, Context>>,
+) {
+    return class MeasuredValueEditor extends React.Component<MeasuredValueEditorProps<Value, Unit, Context>> {
+        render() {
+            const { measured: { value, unit }, context, onChange } = this.props;
+            return <span>
+                <ValueEditor
+                    value={value}
+                    context={context}
+                    onChange={value => onChange({ value, unit })}
+                /> <UnitEditor
+                    value={unit}
+                    context={context}
+                    onChange={unit => onChange({ value, unit })}
+                /></span>;
+        }
+    }
+}
