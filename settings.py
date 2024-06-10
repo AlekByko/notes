fbaf8fc8f7291df46b3aee019fc4cf3413167c5e
AlekByko <>
2024-06-09 22:10:48 -0500

    sampling on saved weights

--- settings.py
+++ settings.py
@@ -4,9 +4,8 @@ import argparse
 def read_settings():
     parser = argparse.ArgumentParser()
     parser.add_argument("--path", type=str)
-    parser.add_argument("--width", type=int)
-    parser.add_argument("--height", type=int)
     parser.add_argument("--mode", type=str)
+    parser.add_argument("--sample-size", type=int)
     args = parser.parse_args()
     settings = Settings(args)
     return settings
@@ -17,20 +16,28 @@ class Settings:
         self.args = args
 
     @property
-    def path(self):
+    def path(self) -> str:
         value = self.args.path
         if (value.find('.') > 0):
             raise Exception(f"Bad path: {value}")
         return value
 
     @property
-    def width(self):
-        return self.args.width
+    def config_path(self):
+        return self.path + '.json'
 
     @property
-    def height(self):
-        return self.args.height
+    def image_path(self):
+        return self.path + '.jpg'
+
+    @property
+    def weights_path(self):
+        return self.path + '.hdf5'
 
     @property
     def mode(self) -> str:
         return self.args.mode
+
+    @property
+    def sample_size(self) -> int:
+        return self.args.sample_size
