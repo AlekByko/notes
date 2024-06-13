7465d802aa4edc9cc74801f04c0d26a17530fd1d
AlekByko <>
2024-06-12 20:03:01 -0500

    experiments with the size of latent vector

--- autoencoder.py
+++ autoencoder.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+from keras import layers
 
 
 def make_coders():
@@ -6,43 +7,48 @@ def make_coders():
     by2x2 = (2, 2)
     by3x2 = (3, 2)
     by3x3 = (3, 3)
-    latent_dim = 64 # used to be 64 with 3 covn layers, now just 2
+    latent_dim = 64 #  loss: 0.0025
+    # latent_dim = 128 #  loss: 0.0023
+    # latent_dim = 256 #  loss: 0.0023
 
     # Define the encoder
-    encoder_input = tf.keras.layers.Input(shape=(180, 320, 3), name='encoder_input')
+    encoder_input = layers.Input(shape=(180, 320, 3), name='encoder_input')
 
-    x = tf.keras.layers.Conv2D(32, by3x3, activation='relu', padding='same')(encoder_input)
-    x = tf.keras.layers.MaxPooling2D(by2x2, padding='same')(x)
+    x = layers.Conv2D(32, by3x3, activation='relu', padding='same')(encoder_input)
+    x = layers.MaxPooling2D(by2x2, padding='same')(x)
 
-    x = tf.keras.layers.Conv2D(64, by3x3, activation='relu', padding='same')(x)
-    x = tf.keras.layers.MaxPooling2D(by3x2, padding='same')(x) # <---- 3x2
+    x = layers.Conv2D(64, by3x3, activation='relu', padding='same')(x)
+    x = layers.MaxPooling2D(by3x2, padding='same')(x) # <---- 3x2
 
     # CNN 128 should stay:
-    x = tf.keras.layers.Conv2D(128, by3x3, activation='relu', padding='same')(x)
-    x = tf.keras.layers.MaxPooling2D(by2x2, padding='same')(x)
+    x = layers.Conv2D(128, by3x3, activation='relu', padding='same')(x)
+    x = layers.MaxPooling2D(by2x2, padding='same')(x)
 
-    x = tf.keras.layers.Flatten()(x)
-    encoder_output = tf.keras.layers.Dense(latent_dim, name='encoder_output')(x)
+
+    x = layers.Flatten()(x)
+
+    # x = layers.Dropout(rate=0.3)(x) # <--- doesn't help
+    encoder_output = layers.Dense(latent_dim, name='encoder_output')(x)
 
     encoder = tf.keras.models.Model(encoder_input, encoder_output, name='encoder')
     encoder.summary()
 
     # Define the decoder
-    decoder_input = tf.keras.layers.Input(shape=(latent_dim,), name='decoder_input')
-    x = tf.keras.layers.Dense(15 * 40 * 128, activation='relu')(decoder_input)
-    x = tf.keras.layers.Reshape((15, 40, 128))(x)
+    decoder_input = layers.Input(shape=(latent_dim,), name='decoder_input')
+    x = layers.Dense(15 * 40 * 128, activation='relu')(decoder_input)
+    x = layers.Reshape((15, 40, 128))(x)
 
     # CNN 128 should stay:
-    x = tf.keras.layers.Conv2DTranspose(128, by3x3, activation='relu', padding='same')(x)
-    x = tf.keras.layers.UpSampling2D(by2x2)(x)
+    x = layers.Conv2DTranspose(128, by3x3, activation='relu', padding='same')(x)
+    x = layers.UpSampling2D(by2x2)(x)
 
-    x = tf.keras.layers.Conv2DTranspose(64, by3x3, activation='relu', padding='same')(x)
-    x = tf.keras.layers.UpSampling2D(by3x2)(x) # <---- 3x2
+    x = layers.Conv2DTranspose(64, by3x3, activation='relu', padding='same')(x)
+    x = layers.UpSampling2D(by3x2)(x) # <---- 3x2
 
-    x = tf.keras.layers.Conv2DTranspose(32, by3x3, activation='relu', padding='same')(x)
-    x = tf.keras.layers.UpSampling2D(by2x2)(x)
+    x = layers.Conv2DTranspose(32, by3x3, activation='relu', padding='same')(x)
+    x = layers.UpSampling2D(by2x2)(x)
 
-    decoder_output = tf.keras.layers.Conv2DTranspose(3, by3x3, activation='sigmoid', padding='same')(x)
+    decoder_output = layers.Conv2DTranspose(3, by3x3, activation='sigmoid', padding='same')(x)
     decoder = tf.keras.models.Model(decoder_input, decoder_output, name='decoder')
 
     autoencoder = tf.keras.models.Model(encoder_input, decoder(encoder_output), name='autoencoder')
