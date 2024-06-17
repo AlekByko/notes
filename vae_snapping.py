4c2510601d958b40a1524d4f6b6948c481566713
AlekByko <>
2024-06-16 20:48:15 -0500

    files renamed

---
+++ vae_snapping.py
@@ -0,0 +1,61 @@
+import numpy as np
+from PIL import Image
+
+from config import load_config, save_config
+from gpu import reset_gpu
+from loading import load_tiles_from_image_ext
+from settings import Settings
+from vae import make_vae
+
+
+def run_vae_snapping_for_one(args: Settings):
+    reset_gpu()
+
+    vae, encoder, decoder = make_vae()
+
+    #vae.summary()
+
+    vae.load_weights(args.weights_path)
+
+    return run_vae_single_snapping(args.image_path, args.config_path, encoder, decoder, args.snap_path)
+
+def run_vae_snapping_for_all(args: Settings):
+    reset_gpu()
+
+    vae, encoder, decoder = make_vae()
+    #vae.summary()
+
+    vae.load_weights(args.weights_path)
+
+    for snap in args.snaps:
+        run_vae_single_snapping(snap.image_path, snap.config_path, encoder, decoder, snap.snap_path)
+
+def run_vae_single_snapping(image_path: str, config_path: str, encoder, decoder, snap_path: str):
+
+    img = Image.open(image_path)
+    config = load_config(config_path)
+    tiles = load_tiles_from_image_ext(img, config)
+
+    white = (255, 255, 255)
+    snap = Image.new('RGB', img.size, white)
+
+    # z_mean, z_log_var, latents = encoder.predict(tiles)
+    latents = encoder.predict(tiles)
+
+    caps = decoder.predict(latents)
+
+    for i in range(len(config.tiles)):
+        tile_config = config.tiles[i]
+        latent = latents[i]
+        tile_config.data['latent'] = latent.tolist()
+        cap_data = caps[i]
+        cap_data = (cap_data * 255).astype(np.uint8)
+        cap_image = Image.fromarray(cap_data, 'RGB')
+        pos = (tile_config.x, tile_config.y)
+        snap.paste(cap_image, pos)
+
+    snap.save(snap_path)
+    save_config(config_path, config)
+    print(snap_path)
+
+
---
+++ vae_traning.py
@@ -0,0 +1,37 @@
+
+import numpy as np
+
+from gpu import reset_gpu
+from loading import load_tiles_from_snaps
+from settings import Settings
+from showing import show
+from vae import make_vae
+
+
+def run_vae_training(args: Settings):
+    reset_gpu()
+
+    tiles = load_tiles_from_snaps(args.snaps)
+
+    np.random.shuffle(tiles)
+
+    #split_at = int(0.8 * len(tiles))
+    train_tiles = tiles#[:split_at]
+    test_tiles = tiles#[split_at:]
+
+    print(f"train length: {len(train_tiles)}, test length: {len(test_tiles)}")
+
+    vae, encoder, decoder = make_vae()
+
+    vae.fit(
+        train_tiles,
+        train_tiles,
+        epochs=args.epochs,
+        batch_size=args.batch,
+        shuffle=True,
+        # validation_data=(test_tiles, test_tiles), # <-- only for keeping the eye on how well model converges
+    )
+
+    vae.save_weights(args.weights_path, overwrite=True)
+
+
