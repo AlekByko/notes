455100ed74215041a96d647209c2aa4c8973c656
AlekByko <>
2024-08-16 17:51:11 -0500

    picking caps dir

--- saving/client/file-system-entries.ts
+++ saving/client/file-system-entries.ts
@@ -7,4 +7,5 @@ export interface KnownPickedDirEntry {
 
 export const knownBaseDirRef = 'base-dir' as KnownPickedDirRef;
 export const knownSnapsDirRef = 'recorded-dir' as KnownPickedDirRef;
+export const knownCapsDirRef = 'caps-dir' as KnownPickedDirRef;
 export const knownTrainingSetDirRef = 'known-training-set-dir' as KnownPickedDirRef;
