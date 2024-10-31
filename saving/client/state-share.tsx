a256a73174fdac073e929c3a02012e982a09f12b
AlekByko <>
2024-10-31 18:20:28 -0500

    sorting works

---
+++ saving/client/state-share.tsx
@@ -0,0 +1,37 @@
+interface State {
+    text: string;
+}
+function thusTest(text: string) {
+    return class Test extends React.Component<{}, State> {
+        state = { text }
+        render() {
+            const { text } = this.state;
+            return <a href="#" onClick={e => {
+                e.preventDefault();
+                e.stopPropagation();
+                this.setState({ text: Math.random().toString() })
+            }}>{text}</a>
+        }
+    }
+}
+
+function thusApp() {
+    const Test1 = thusTest('a');
+    const Test2 = thusTest('b');
+    return class App extends React.Component<{}> {
+        render(): React.ReactNode {
+            return <div>
+                {Math.random() ? <Test1 /> : <Test2 />}
+                <div><button onClick={() => {
+                    this.forceUpdate();
+                }}>Toss</button></div>
+            </div>;
+        }
+    }
+}
+
+if (window.sandbox === 'state-share') {
+    const App = thusApp();
+    const rootElement = document.getElementById('root');
+    ReactDOM.render(<App />, rootElement);
+}
