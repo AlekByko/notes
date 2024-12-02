553715134c4dad3880976dd8745607a05fb07bd5
AlekByko <>
2024-12-01 22:36:35 -0500

    tracked number employed

--- saving/client/tracked-number.tsx
+++ saving/client/tracked-number.tsx
@@ -1,27 +1,36 @@
 import React from 'react';
 
-export interface TrackedNumberProps {
-    value: number;
+export interface TrackedNumberProps<Holder> {
+    holder: Holder;
 }
 
 interface State {
-    lastValue: number;
-    delta: number;
+    lastValue: number ;
+    delta: number ;
 }
 
-export function thusTrackedNumber() {
+export function thusTrackedNumber<Holder>(
+    defaults: {
+        numberOf: (holder: Holder) => number,
+        formatNumber: (value: number) => string,
+    },
+) {
 
-    type Props = TrackedNumberProps;
+    type Props = TrackedNumberProps<Holder>;
 
     function makeDefaultState(props: Props): State {
-        return { lastValue: props.value, delta: 0 };
+        const { holder } = props;
+        const value = defaults.numberOf(holder);
+        return { lastValue: value, delta: 0 };
     }
 
     return class TrackedNumber extends React.Component<Props, State> {
         state = makeDefaultState(this.props);
         static getDerivedStateFromProps(props: Props, state: State): State | null {
-            const delta = state.lastValue - props.value;
-            return { ...state, lastValue: props.value, delta } satisfies State;
+            const { holder } = props;
+            const value = defaults.numberOf(holder);
+            const delta = state.lastValue - value;
+            return { ...state, lastValue: value, delta } satisfies State;
         }
         render() {
             const { delta, lastValue } = this.state;
