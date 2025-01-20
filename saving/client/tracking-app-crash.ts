6ac171457e8209338c781c443ec4d835f61c4f31
AlekByko <>
2025-01-19 21:36:30 -0500

    tracking full stop application crashes

---
+++ saving/client/tracking-app-crash.ts
@@ -0,0 +1,25 @@
+import { formatElapsedToMinutes, toElapsedFromMiliseconds } from '../shared/time-elapsed';
+import { toTimestamp } from '../shared/time-stamping';
+
+export function enableAppCrashTracking(window: Window): Act {
+    const whenError = (e: ErrorEvent) => {
+        const happendAt = toTimestamp();
+        alert(`!!! UNHANDLED ERROR !!! CHECK THE CONSOLE !!! `);
+        const now = toTimestamp();
+        console.log(e);
+        console.trace();
+        const ago = now - happendAt;
+        const elapsed = toElapsedFromMiliseconds(ago);
+        const elapsedText = formatElapsedToMinutes(elapsed);
+        console.log(`Happend at ${new Date(happendAt).toLocaleString()} it was ago: ${elapsedText}`);
+        try {
+            const { message, source, lineno, colno, error, stack } = e.error;
+            console.log({ message, source, lineno, colno, error, stack });
+        }
+        finally { }
+    };
+    window.addEventListener('error', whenError);
+    return () => {
+        window.removeEventListener('error', whenError);
+    }
+}
