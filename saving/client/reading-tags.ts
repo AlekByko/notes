c49359e28d8190fc1814049db3f4bba79e98c549
AlekByko <>
2025-06-26 17:41:58 -0500

    reading list of tags

---
+++ saving/client/reading-tags.ts
@@ -0,0 +1,26 @@
+import { isNonNull, isNull } from '../shared/core';
+
+export function readTagsInQueryString(text: string) {
+    const regex = /[&?]tags=([\w\-_]+(,[\w\-_]+)+)/ig;
+    const match = regex.exec(text);
+    if (isNull(match)) return null;
+    const [_full, textList] = match;
+    return _readTagsInList(textList);
+}
+
+/** UNSAFE: no check that comma is a separator */
+function _readTagsInList(text: string) {
+    const reg = /[\w\-_]+/g;
+    const tags: string[] = [];
+    for (let match = reg.exec(text); isNonNull(match); match = reg.exec(text)) {
+        const [tag] = match;
+        tags.push(tag);
+    }
+    return tags;
+}
+
+if (window.sandbox === 'reading-tags') {
+    const tags = readTagsInQueryString('fhsdkjfhskj?tags=test,hey,what-up,_no_comments');
+    console.log(tags);
+}
+
