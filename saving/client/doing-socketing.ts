162b1e1501cf80efcb099de895913821512034f2
AlekByko <>
2025-09-07 21:18:27 -0500

    setting up the socket client moved out

--- saving/client/doing-socketing.ts
+++ saving/client/doing-socketing.ts
@@ -1,3 +1,4 @@
+
 export function willOpenSocket(port: number): Promise<WebSocket> {
     const socket = new WebSocket('ws://localhost:' + port);
     return new Promise<WebSocket>((resolve, reject) => {
@@ -12,3 +13,44 @@ export function willOpenSocket(port: number): Promise<WebSocket> {
         }
     });
 }
+
+export function whenClosedOrErroredSocketOver(
+    takeSocket: (socket: WebSocket) => void,
+    socketPort: number,
+    whenSocketMessage: (e: MessageEvent<any>) => void,
+) {
+    return async function whenClosedOrErroredSocket(e: Event) {
+        console.log(e);
+        alert('SOCKET CLOSED!');
+        function scheduleTryReopen(delay: number): void {
+            setTimeout(async () => {
+                willTryReopen();
+            }, delay);
+        }
+        const retryInThatManySeconds = 10;
+        const retryDelay = retryInThatManySeconds * 1000;
+        let attemptCounter = 0;
+        async function willTryReopen() {
+            try {
+                const socket = await willOpenSocket(socketPort);
+                socket.onmessage = whenSocketMessage;
+                socket.onclose = whenClosedOrErroredSocket;
+                socket.onerror = whenClosedOrErroredSocket;
+                takeSocket(socket);
+                alert('SOCKET REOPENED');
+                window.document.title = `SOCKET REOPENED after #${attemptCounter} attempts`;
+            } catch (e) {
+                debugger;
+                console.log(e);
+                if (attemptCounter < 1) {
+                    alert(`UNABLE TO REOPEN THE SOCKET!!!! CHECK THE CONSOLE!!!`);
+                }
+                attemptCounter += 1;
+                window.document.title = `#${attemptCounter}: UNABLE TO REOPEN THE SOCKET!!!! CHECK THE CONSOLE!!!`;
+                scheduleTryReopen(retryDelay);
+            }
+        }
+        scheduleTryReopen(retryDelay);
+    };
+}
+
