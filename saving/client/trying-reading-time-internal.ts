2951f78da99a5d0abc153c4e9d433c9a194c9828
AlekByko <>
2025-09-19 18:55:17 -0500

    formatting time interval

--- saving/client/trying-reading-time-internal.ts
+++ saving/client/trying-reading-time-internal.ts
@@ -1,5 +1,6 @@
-import { alwaysNull } from '../shared/core';
+import { alwaysNull, isNull, run } from '../shared/core';
 import { parseTimeInternalOr } from '../shared/reading-time-interval';
+import { formatTimeInterval, getMsOfTimeInterval, makeTimeIntervalOfMs } from '../shared/time-interval';
 
 if (window.sandbox === 'trying-reading-time-internal') {
     console.log(parseTimeInternalOr(`1d`, alwaysNull));
@@ -15,4 +16,17 @@ if (window.sandbox === 'trying-reading-time-internal') {
     console.log(parseTimeInternalOr(`4s 5ms`, alwaysNull));
 
     console.log(parseTimeInternalOr(`1d 2h 3m 4s 5ms`, alwaysNull));
+
+    run(() => {
+        const inputText = `1d 2h 3m 4s 5ms`;
+        const inputInterval = parseTimeInternalOr(inputText, alwaysNull);
+        if (isNull(inputInterval)) return console.log(`Bad text: ${inputText}`);
+
+        const ms = getMsOfTimeInterval(inputInterval);
+        console.log(ms);
+        const outputInterval = makeTimeIntervalOfMs(ms);
+        console.log(outputInterval);
+        const outputText = formatTimeInterval(outputInterval);
+        console.log('identity text', { inputText, inputInterval, ms, outputInterval, outputText });
+    });
 }
--- saving/shared/time-interval.ts
+++ saving/shared/time-interval.ts
@@ -19,6 +19,26 @@ export function makeTimeIntervalOfMs(ms: number): TimeInterval {
     return { days, hours, minutes, seconds, milliseconds };
 }
 
+export function formatTimeInterval({ days, hours, minutes, seconds, milliseconds }: TimeInterval) {
+    const result: string[] = [];
+    if (days > 0) {
+        result.push(`${days}d`);
+    }
+    if (hours > 0) {
+        result.push(`${hours}h`);
+    }
+    if (minutes > 0) {
+        result.push(`${minutes}m`);
+    }
+    if (seconds > 0) {
+        result.push(`${seconds}s`);
+    }
+    if (milliseconds > 0) {
+        result.push(`${milliseconds}ms`);
+    }
+    return result.join(' ');
+}
+
 export function getMsOfTimeInterval(interval: TimeInterval) {
     const { days, hours, minutes, seconds, milliseconds } = interval;
     let internal = 0;
@@ -26,7 +46,7 @@ export function getMsOfTimeInterval(interval: TimeInterval) {
     internal += hours * 1000 * 60 * 60;
     internal += minutes * 1000 * 60;
     internal += seconds * 1000;
-    internal += milliseconds ;
+    internal += milliseconds;
     return internal;
 }
 
