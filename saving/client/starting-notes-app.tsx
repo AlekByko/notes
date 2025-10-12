e3bf64110c42d97edd304d85b0e931791ac44195
AlekByko <>
2025-10-11 21:44:08 -0500

    cleanup

--- saving/client/starting-notes-app.tsx
+++ saving/client/starting-notes-app.tsx
@@ -13,26 +13,14 @@ import { willClaimDir } from './setting-up-notes-app';
 
 async function run() {
     const db = await willOpenKnownDb();
-    // const maxCount = readNumberFromQueryString(/[?&]count=(\d+)/ig, 10);
-    // const givenTags = readTagsInQueryString(window.location.search) ?? [];
-    // const matesDir = await willTryGetDirFromDb(db, knownMatesDirRef);
-    // sureNonNull(matesDir, 'pairs dir');
 
     const title = readStringFromQueryStringOr(/[?&]title=([-_\w\d]+)/ig, null);
     if (isNonNull(title)) {
         document.title = title;
     }
 
-
-
     const rootElement = document.getElementById('root')!;
-
-    // const repository = new Repository();
-    // const tracker = makeCamConfigTracker(repository);
-    // const nameBuckets: GlobalNameBuckets = new Map();
-
     const notesDir = await willClaimDir(db, rootElement, knownNotesDirRef);
-
     const glob: NotesGlob = { db, notesDir };
     const drop = new Drop(notesDir, 'test.txt');
     const key = toRandKey();
