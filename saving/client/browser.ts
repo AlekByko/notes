f13ae07cdf37a267a6ad484db7be5e03c2dba8ec
AlekByko <>
2025-10-13 17:30:24 -0500

    asking to save changes before unload in snap app

--- saving/client/browser.ts
+++ saving/client/browser.ts
@@ -1,5 +1,5 @@
 export function enableAskingForUnsavedChanged(window: Window): void {
     window.addEventListener('beforeunload', e => {
-        e.returnValue = 'unsaved changed';
+        return e.returnValue = 'unsaved changed';
     });
 }
