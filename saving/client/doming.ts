09f129573c36309242c35fb1325f3ac093317dd7
AlekByko <>
2025-12-14 13:10:17 -0500

    selecting search result by keyboard

--- saving/client/doming.ts
+++ saving/client/doming.ts
@@ -1,8 +1,12 @@
 import { isUndefined } from '../shared/core';
 
-export function seeIfElement(e: unknown): e is Element {
-    if (isUndefined(e)) return false;
-    if (typeof e !== 'object') return false;
-    if (e !== null && 'nodeType' in e && e.nodeType === Node.ELEMENT_NODE) return true;
-    return false;
+export function seeIfElement(something: unknown): something is Element {
+    if (isUndefined(something)) return false;
+    if (typeof something !== 'object') return false;
+    return something instanceof Element;
+}
+export function seeIfHTMLElement(something: unknown): something is HTMLElement {
+    if (isUndefined(something)) return false;
+    if (typeof something !== 'object') return false;
+    return something instanceof HTMLElement;
 }
--- saving/client/notes-search.tsx
+++ saving/client/notes-search.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useRef, useState } from 'react';
-import { isNull } from '../shared/core';
+import { cast, isNonNull, isNull } from '../shared/core';
 import { CardProps } from './cards';
-import { seeIfElement } from './doming';
+import { seeIfElement, seeIfHTMLElement } from './doming';
 import { CardKey } from './notes-workspace';
 
 export interface NotesSearchProps {
@@ -11,47 +11,131 @@ export interface NotesSearchProps {
     onHide: () => void;
 }
 
-function pullCardKey(target: EventTarget) {
-    if (!seeIfElement(target)) return null;
-    const cardKey = target.getAttribute('data-card-key');
-    return cardKey as CardKey;
-}
 export function thusNotesSearch(delay: number) {
     return function NotesSearch(props: NotesSearchProps) {
-        const lastSearchTextRef = useRef('');
+        const lastTextRef = useRef('');
         const timerRef = useRef(0);
         const [found, setFound] = useState([] as CardProps[]);
-        const lastOveredRef = useRef(null as null | CardKey);
+        const lastCardKeyRef = useRef(null as null | CardKey);
+        const lastElementRef = useRef(null as null | HTMLElement);
+        const lastMouseOverPosRef = useRef(null as null | { x: number, y: number });
 
         const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
             let text = e.currentTarget.value;
             setText(text);
             const searchText = text.trim().toLowerCase();
-            if (searchText === lastSearchTextRef.current || searchText === '') return;
-            lastSearchTextRef.current = searchText;
+            if (searchText === lastTextRef.current || searchText === '') return;
+            lastTextRef.current = searchText;
             window.clearTimeout(timerRef.current);
             timerRef.current = window.setTimeout(() => {
                 const found = props.search(searchText);
+                reset();
                 setFound(found);
             }, delay);
         };
 
+        const reset = () => {
+            if (isNonNull(lastElementRef.current)) {
+                lastElementRef.current.classList.remove('as-highlighted');
+                lastElementRef.current = null;
+            }
+            lastCardKeyRef.current = null;
+            lastMouseOverPosRef.current = null;
+        }
+
         const whenMouseOver = (e: React.MouseEvent<HTMLElement>) => {
-            const cardKey = pullCardKey(e.target);
+            if (isNonNull(lastMouseOverPosRef.current)) {
+                const { x, y } = lastMouseOverPosRef.current;
+                // it means the mouse hasn't moved but another
+                // mouseover occured, it can be when users scrolls
+                // by up/down arrow keys in which case we don't want
+                // this mouseover to happen
+                if (e.pageX === x && e.pageY === y) return;
+            }
+            lastMouseOverPosRef.current = { x: e.pageX, y: e.pageY };
+            if (!seeIfHTMLElement(e.target)) return;
+            const cardKey = e.target.getAttribute('data-card-key');
             if (isNull(cardKey)) return;
-            lastOveredRef.current = cardKey;
+            cast<CardKey>(cardKey);
+            highlight(e.target, cardKey, false);
+        }
+
+        function highlight(element: HTMLElement, cardKey: CardKey, shouldScroll: boolean) {
+            lastCardKeyRef.current = cardKey;
+            if (isNonNull(lastElementRef.current)) {
+                lastElementRef.current.classList.remove('as-highlighted');
+            }
+            lastElementRef.current = element;
+            lastElementRef.current.classList.add('as-highlighted');
+            if (shouldScroll) {
+                element.scrollIntoView({ block: 'center' });
+            }
             props.onPreview(cardKey);
         }
 
         const [text, setText] = useState('');
         const whenMouseClick = (e: React.MouseEvent<HTMLElement>) => {
-            const cardKey = pullCardKey(e.target);
+            if (!seeIfElement(e.target)) return;
+            const cardKey = e.target.getAttribute('data-card-key');
             if (isNull(cardKey)) return;
+            cast<CardKey>(cardKey);
+            reset();
             props.onSelect(cardKey);
+        };
+        function select() {
+            const cardKey = lastCardKeyRef.current;
+            if (isNull(cardKey)) return;
+            reset();
+            props.onSelect(cardKey);
+        }
+        const goUp = (e: React.KeyboardEvent) => {
+            e.preventDefault();
+            if (isNull(resultListElementRef.current)) return;
+            let at = isNonNull(lastCardKeyRef.current)
+                ? found.findIndex(x => x.cardKey === lastCardKeyRef.current)
+                : found.length > 0
+                    ? 0
+                    : -1;
+            if (at < 0) return;
+            if (at > 1) {
+                at -= 1;
+            } else {
+                at = found.length - 1;
+            }
+            const { cardKey } = found[at];
+            const elements = resultListElementRef.current.querySelectorAll("div");
+            const element = elements.item(at);
+            highlight(element, cardKey, true);
+        };
+        const goDown = (e: React.KeyboardEvent) => {
+            e.preventDefault();
+            if (isNull(resultListElementRef.current)) return;
+            let at = isNonNull(lastCardKeyRef.current)
+                ? found.findIndex(x => x.cardKey === lastCardKeyRef.current)
+                : found.length > 0
+                    ? found.length - 1
+                    : -1;
+            if (at < 0) return;
+            if (at < found.length - 1) {
+                at += 1;
+            } else {
+                at = 0;
+            }
+            const { cardKey } = found[at];
+            const elements = resultListElementRef.current.querySelectorAll("div");
+            const element = elements.item(at);
+            highlight(element, cardKey, true);
         };
 
         const onKeyDown = (e: React.KeyboardEvent<HTMLElement>) => {
-            if (e.nativeEvent.code === 'Escape') props.onHide();
+            const { code } = e.nativeEvent;
+            switch (code) {
+                case 'Escape': return props.onHide();
+                case 'ArrowUp': return goUp(e);
+                case 'ArrowDown': return goDown(e);
+                case 'Enter': return select();
+                default: break; // do nothing
+            }
         };
 
         const inputRef = useRef(null as HTMLInputElement | null);
@@ -61,11 +145,13 @@ export function thusNotesSearch(delay: number) {
             inputRef.current.focus();
         }, []);
 
+        const resultListElementRef = useRef(null as HTMLDivElement | null);
+
         return <div className="notes-search">
             <div>
                 <input className="notes-search-input" value={text} onChange={onChange} onKeyDown={onKeyDown} ref={inputRef} />
             </div>
-            <div className="notes-search-results" onMouseOver={whenMouseOver} onClick={whenMouseClick}>
+            <div className="notes-search-results" ref={resultListElementRef} onMouseOver={whenMouseOver} onClick={whenMouseClick}>
                 {found.map(card => {
                     return <div key={card.cardKey} data-card-key={card.cardKey} className="notes-search-result">{card.title}</div>;
                 })}
